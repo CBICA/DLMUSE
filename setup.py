ce7a571039f34a8ca7f75ce032039e79e1f8e1f0
Spiros Maggioros <spirosastro@gmail.com>
2024-11-10 01:18:43 +0200

    Fixed torch version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(exclude=["tests", ".github"]),
     python_requires=">=3.8",
     install_requires=[
-        "torch<=2.3.1",
+        "torch<=2.2.1",
         "nnunetv2",
         "argparse",
         "huggingface_hub"
